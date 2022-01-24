1f4f5cef9f86f8315042702335b4703271407a21
Bob Merullo <bmerullo85@gmail.com>
2022-01-24 11:35:23 -0700

    Connected Database

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}))
+
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:3000",
+    }),
+);
+
+require("./config/mongoose.config.js");
+// require("./routes/player.routes")(app);
+
+
+app.listen (8000, ()=> console.log("you are conntected at port 8000"));
